db71f8e428f541a36b9d55a9140298c5f0398e23
Пользователь <valetmail@gmail.com>
2019-11-30 00:04:49 +0300

    Initial commit

---
+++ react_front_end/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h2>Start</h2>
+    </div>
+  );
+}
+
+export default App;
